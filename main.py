197696275b6a3879fb21d724ae085131d19aacc7
wwatso11 <wwatso11@wgu.edu.com>
2026-04-20 20:56:14 +0000

    Data Check

--- main.py
+++ main.py
@@ -71,7 +71,18 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(),"src","data_check"),
+                "main",
+                env_manager="conda",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )            
 
         if "data_split" in active_steps:
             ##################
